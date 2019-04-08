6f5504f7ee01254d510a90bed3302f9f6005ba63
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-04-08 12:56:15 -0300

    Java UI Design

---
+++ RegisterPage/src/registerpage/RegisterPage.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registerpage;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class RegisterPage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Registro rg = new Registro();
+        rg.setVisible(true);
+        
+    }
+    
+}
